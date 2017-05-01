cb37f416c162b4414d6eb79731167632ad302e82
GalinStoychev <stoychev.galin@gmail.com>
2017-05-01 22:32:42 +0300

    refactoring variables

--- githubApi/app/controllers/api_module.rb
+++ githubApi/app/controllers/api_module.rb
@@ -1,8 +1,6 @@
 module ApiModule
   extend ActiveSupport::Concern
 
-    WATCHERS_COUNT = "watchers_count"
-    
     included do
         # class level code
         # before_filter ....
@@ -12,10 +10,10 @@ module ApiModule
         result = get_top_five_repos(username)
         if (result[:is_success])
             repositories_to_add = map_repos(result[:data])
-            @user_from_db = User.new(username: username, repositories: repositories_to_add)
+            user_from_db = User.new(username: username, repositories: repositories_to_add)
 
-            if  (@user_from_db.save)
-                return {:is_success => true, :data => @user_from_db}
+            if  (user_from_db.save)
+                return {:is_success => true, :data => user_from_db}
             else
                 return {:is_success => false}
             end
@@ -25,19 +23,19 @@ module ApiModule
     end
     
     def get_user(username)
-        @user_from_db = User.find_by(username: username)
-        seconds_diff = time_diff(@user_from_db[:updated_at].to_i, Time.now.to_i)
+        user_from_db = User.find_by(username: username)
+        seconds_diff = time_diff(user_from_db[:updated_at].to_i, Time.now.to_i)
         
         if (seconds_diff > 5 * 60 ) #more than 5 min
             result = get_top_five_repos(username)
             repositories_to_add = map_repos(result[:data])
-            if (@user_from_db.update_attributes!(repositories: repositories_to_add))
-                return {:is_success => true, :data => @user_from_db}
+            if (user_from_db.update_attributes!(repositories: repositories_to_add))
+                return {:is_success => true, :data => user_from_db}
             else
                 return {:is_success => false}
             end
         else #less than 5 min
-            return {:is_success => true, :data => @user_from_db}
+            return {:is_success => true, :data => user_from_db}
         end
     end
 
@@ -56,7 +54,7 @@ module ApiModule
         github = Github.new()
         user_repos = github.repositories(username)
         begin
-            data = user_repos.sort_by { |repo| repo[WATCHERS_COUNT] }.reverse.take(5)
+            data = user_repos.sort_by { |repo| repo["watchers_count"] }.reverse.take(5)
         rescue
             return {:is_success => false}
         else
@@ -66,9 +64,7 @@ module ApiModule
 
     def time_diff(start_time, end_time)
       seconds_diff = (end_time - start_time)
-      puts seconds_diff
+      # puts seconds_diff
       return seconds_diff
     end
-
-  # instance methods here
 end
